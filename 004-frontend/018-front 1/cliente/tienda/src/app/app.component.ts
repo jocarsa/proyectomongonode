b0f8b704552b17b9dc735fa8af77f7b964938d33
jocarsa <info@josevicentecarratala.com>
2024-11-05 10:16:09 +0100

    3

--- 004-frontend/018-front 1/cliente/tienda/src/app/app.component.ts
+++ 004-frontend/018-front 1/cliente/tienda/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { HeroeComponent } from './heroe/heroe.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { PiedepaginaComponent } from './piedepagina/piedepagina.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CabeceraComponent,
+    HeroeComponent,
+    PrincipalComponent,
+    PiedepaginaComponent
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
