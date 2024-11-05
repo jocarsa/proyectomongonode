c77775db5fb2621ada3fbccad31ed85d1a2d237c
jocarsa <info@josevicentecarratala.com>
2024-11-05 10:43:12 +0100

    7

--- 004-frontend/018-front 1/cliente/tienda/src/app/principal/principal.component.ts
+++ 004-frontend/018-front 1/cliente/tienda/src/app/principal/principal.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common'; // Importar CommonModule
 import { DameproductosService } from '../dameproductos.service';
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [HttpClientModule], // Import HttpClientModule directly here
+  imports: [HttpClientModule, CommonModule], // Import HttpClientModule directly here
   templateUrl: './principal.component.html',
   styleUrls: ['./principal.component.css']
 })
@@ -21,7 +22,7 @@ export class PrincipalComponent implements OnInit {
   dameDatos() {
     this.dameproductosService.dameDatos().subscribe(datos => {
       this.jsonData = datos;
-      console.log(datos);
+      
     });
   }
 }
