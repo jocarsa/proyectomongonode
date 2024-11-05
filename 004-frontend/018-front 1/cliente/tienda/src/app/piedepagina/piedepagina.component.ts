2571f48db42d3605dd5bbbec857d5afe1b036b40
jocarsa <info@josevicentecarratala.com>
2024-11-05 10:04:08 +0100

    2

---
+++ 004-frontend/018-front 1/cliente/tienda/src/app/piedepagina/piedepagina.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-piedepagina',
+  standalone: true,
+  imports: [],
+  templateUrl: './piedepagina.component.html',
+  styleUrl: './piedepagina.component.css'
+})
+export class PiedepaginaComponent {
+
+}
